e2463296275f3b040932c2811f784f156085a7b8
adamochi <honeyrumballs@gmail.com>
2022-07-23 21:23:06 +1000

    disaster has been averted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ConfirmDelete from "./hooks/useConfirm";
+import PreventLeave from "./hooks/usePreventLeave";
 
 function App() {
   return (
@@ -8,17 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ConfirmDelete />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br />
+        <PreventLeave />
+        <br />
       </header>
     </div>
   );
--- src/hooks/useConfirm.js
+++ src/hooks/useConfirm.js
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 const useConfirm = (message = "", onConfirm, onCancel) => {
   if (!onConfirm || typeof onConfirm !== "function") {
     return;
@@ -15,12 +17,20 @@ const useConfirm = (message = "", onConfirm, onCancel) => {
 };
 
 export default function ConfirmDelete() {
-  const deleteWorld = () => console.log("deleting the world...");
-  const abort = () => console.log("disaster has been averted");
+  const deleted = useRef();
+  const deleteWorld = () => {
+    deleted.current.innerText = "world is being deleted. . .";
+    console.log("world is being deleted...");
+  };
+  const abort = () => {
+    deleted.current.innerText = "disaster has been averted";
+    console.log("disaster has been averted");
+  };
   const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
   return (
     <div className="App">
       <button onClick={confirmDelete}>Delete the world</button>
+      <h2 ref={deleted}>hi</h2>
     </div>
   );
 }
